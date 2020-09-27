bdf33facad0c174c146e123c26fe912b48207d73
Jotunbane <PawMikolajczak@gmail.com>
2020-09-27 14:45:50 +0200

    Displaying date

--- src/basic/Field.jsx
+++ src/basic/Field.jsx
@@ -4,8 +4,9 @@ import {Button} from "./Button";
 export function Field(props){
     const fieldState = (props.task.done);
 
-    return <div className={"field " +(fieldState && "done")}>
-        {props.task.text}
+    return <div className={"field " + (fieldState && "done")}>
+        {props.task.text+" "}
+        {props.task.date}
         <Button task={props.task} onChange={props.onChange}/>
 
     </div>
