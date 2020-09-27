6af3b03a25d965487916b94cb65076716aa01bf4
nexTuptus <bartekmoskala@gmail.com>
2020-09-27 11:33:01 +0200

    Added Button

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import React from 'react';
 import './basic/style/style.css'
 import './App.css';
 import {List} from "./basic/List";
+import {Button} from "./basic/Button";
 
 function App() {
-  return <List/>
+  return <Button/>
 
 }
 
