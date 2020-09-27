cd3d131fc9f83b629fae74f8354fa10887cdaffe
Jotunbane <PawMikolajczak@gmail.com>
2020-09-27 10:13:13 +0200

    Szkielet pola

---
+++ src/basic/pole.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export function GenerujPole(ifDeadline){
+    const flag = ifDeadline.flag;
+
+    return <div className={"pole"}>
+        <input type={"textarea"}/>
+
+
+    </div>
+
+
+}
