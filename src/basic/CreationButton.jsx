312bd08f4c7d7c9e208fba71e486c08dbe035785
nexTuptus <bartekmoskala@gmail.com>
2020-09-27 14:42:16 +0200

    added date input to CreationButton.jsx

--- src/basic/CreationButton.jsx
+++ src/basic/CreationButton.jsx
@@ -2,17 +2,21 @@ import React from "react";
 
 export function CreationButton(props) {
     const [text, setText] = React.useState("");
+    const [date, setDate] = React.useState(0);
 
     function addTask() {
         props.onCreate({
             done: false,
-            text: text
-        });
+            text: text,
+            date: date
+        })
+        setText("");
     }
 
     return <div>
 
         <input placeholder="enter task" value={text} onChange={(event => setText(event.currentTarget.value))}/>
+        <input type={"date"} value={date} onChange={(event => setDate(event.currentTarget.value))}/>
         <button onClick={addTask} disabled={text===""}>add</button>
 
 
--- src/basic/List.jsx
+++ src/basic/List.jsx
@@ -3,11 +3,7 @@ import {Field} from "./Field";
 import {CreationButton} from "./CreationButton";
 
 export function List(props) {
-    const[tasks, setTasks] = React.useState([
-        {text: 'Ala', done: false},
-        {text: 'ma', done: false},
-        {text: 'kota', done: true}
-    ]);
+    const[tasks, setTasks] = React.useState([]);
 
     function handleChange(task) {
         task.done = !task.done;
